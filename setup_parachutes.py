40f6d50b1a375a4c2822f2c8bc7e2c8b1f1f5c25
alorsunique <sounique7800@gmail.com>
2023-12-20 18:33:41 +0800

    2023 December 20 Added setup file

---
+++ setup_parachutes.py
@@ -0,0 +1,19 @@
+import os
+from pathlib import Path
+
+resources_dir_text = "Resources_Path.txt"
+
+with open(resources_dir_text, 'a') as writer:
+    writer.close()
+
+entry_list = []
+
+with open(resources_dir_text, 'r') as reader:
+    entry_list.append(reader.read())
+    reader.close()
+
+resources_dir = Path(entry_list[0])
+print(f"Resources Directory: {resources_dir}")
+
+if not resources_dir.exists():
+    os.mkdir(resources_dir)
